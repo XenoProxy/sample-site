4bdbfc7ea4b017962dab3d886ce3b389775bfc85
XenoProxy <ksenomorph6@gmail.com>
2021-08-08 00:18:54 +0300

    [+] admin model editor

--- bboard/admin.py
+++ bboard/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 from .models import Bd
 
 
-admin.site.register(Bd)
+class BdAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published')  # посл-ть имён полей, кот. выводятся в списке записей
+    list_display_links = ('title', 'content')  # посл-ть имён полей, кот. должны преобр-ны в гиперссылки, ведущие на стр. правки записи
+    search_fields = ('title', 'content', )  # посл-ть имён полей, по кот. должна вып-ся фильтрация
+
+
+admin.site.register(Bd, BdAdmin)
