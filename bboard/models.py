3ceeb0d7301588f0641fbbbd5bd2a8af58621d8a
XenoProxy <ksenomorph6@gmail.com>
2021-08-07 17:42:04 +0300

    [+] First migrations

--- bboard/models.py
+++ bboard/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Bd(models.Model):
-    title = models.CharField(max_length=50),
-    content = models.TextField(null=True, blank=True),
-    price = models.FloatField(null=True, blank=True),
+    title = models.CharField(max_length=50, null=True)
+    content = models.TextField(null=True, blank=True)
+    price = models.FloatField(null=True, blank=True)
     published = models.DateTimeField(auto_now_add=True, db_index=True)
