2851dad196680a0fbc83f8fcd27b06a021b05933
XenoProxy <ksenomorph6@gmail.com>
2021-08-08 00:08:25 +0300

    [*] improved models

--- bboard/models.py
+++ bboard/models.py
@@ -2,7 +2,20 @@ from django.db import models
 
 
 class Bd(models.Model):
-    title = models.CharField(max_length=50, null=True)
-    content = models.TextField(null=True, blank=True)
-    price = models.FloatField(null=True, blank=True)
-    published = models.DateTimeField(auto_now_add=True, db_index=True)
+    title = models.CharField(
+        max_length=50, null=True, verbose_name='Product'
+    )
+    content = models.TextField(
+        null=True, blank=True, verbose_name='Description'
+    )
+    price = models.FloatField(
+        null=True, blank=True, verbose_name='Price'
+    )
+    published = models.DateTimeField(
+        auto_now_add=True, db_index=True, verbose_name='Published'
+    )
+
+    class Meta:  # Служит для описания параметров самой модели
+        verbose_name_plural = 'Advertisements'
+        verbose_name = 'Advertisement'
+        ordering = ['-published']  # сортировка по убыванию даты публикации
--- bboard/views.py
+++ bboard/views.py
@@ -4,5 +4,5 @@ from .models import Bd
 
 
 def index(request):
-    bbs = Bd.objects.order_by('-published')  # сортировка по убыванию даты публикации
+    bbs = Bd.objects.all()
     return render(request, 'index.html', {'bbs': bbs})  # загрузка и рендер страницы
