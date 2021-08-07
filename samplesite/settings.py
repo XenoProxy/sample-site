fa8088dcad10508db93197b08544ff4daa368e47
XenoProxy <ksenomorph6@gmail.com>
2021-08-07 16:17:50 +0300

    [+] App has been to the project settings and URLs list

--- samplesite/settings.py
+++ samplesite/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bboard.apps.BboardConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bboard.apps.BboardConfig',
 ]
 
 MIDDLEWARE = [
