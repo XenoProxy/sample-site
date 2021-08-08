977bcae530627afd98f7915ca40db9d31d29cadf
XenoProxy <ksenomorph6@gmail.com>
2021-08-08 20:52:32 +0300

    [+] Rubric's view, urls and fixed index.html

--- bboard/views.py
+++ bboard/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
-from .models import Bd
+from .models import Bd, Rubric
 
 
 def index(request):
     bbs = Bd.objects.all()
-    return render(request, 'index.html', {'bbs': bbs})  # загрузка и рендер страницы
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'index.html', context)  # загрузка и рендер страницы
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bd.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'by_rubric.html', context)
