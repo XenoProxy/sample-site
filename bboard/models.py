1c5d1ba9b5bf2f34e395d17b852492f0c4dff5c9
XenoProxy <ksenomorph6@gmail.com>
2021-08-08 14:44:24 +0300

    [+] model Rubric

--- bboard/models.py
+++ bboard/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Name')
+
+    class Meta:
+        verbose_name_plural = 'Rubrics'
+        verbose_name = 'Rubric'
+        ordering = ['name']
+
+
 class Bd(models.Model):
     title = models.CharField(
         max_length=50, null=True, verbose_name='Product'
@@ -13,6 +22,9 @@ class Bd(models.Model):
     )
     published = models.DateTimeField(
         auto_now_add=True, db_index=True, verbose_name='Published'
+    )
+    rubric = models.ForeignKey(
+        'Rubric', null=True, on_delete=models.PROTECT, verbose_name='Rubric'
     )
 
     class Meta:  # Служит для описания параметров самой модели
